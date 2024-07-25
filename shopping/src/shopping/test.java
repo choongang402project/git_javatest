4ed1ffdf04f3ea5c2593fef1df19cd60dd91fa08
tjdals2597 <tjdals2597@naver.com>
2024-07-25 16:45:21 +0900

    45

--- shopping/src/shopping/test.java
+++ shopping/src/shopping/test.java
@@ -3,11 +3,11 @@ package shopping;
 public class test {
 
 	public static void main(String[] args) {
-			String mid= "�솉湲몃룞";
+			String mid= "홍길동";
 			int a= 10;
 			int b= 25;
 			System.out.println(mid);
-			System.out.println("�븞�뀞");
+			System.out.println("안녕");
 			String slf="sdfnaonwo";
 	}
 
