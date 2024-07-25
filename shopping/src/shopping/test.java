0ad2c6cd59d3c9979df6d9522ec44e04c64cc768
hallowgred <rkdgnl365@naver.com>
2024-07-25 16:43:51 +0900

    asdasd

--- shopping/src/shopping/test.java
+++ shopping/src/shopping/test.java
@@ -3,11 +3,12 @@ package shopping;
 public class test {
 
 	public static void main(String[] args) {
-			String mid= "홍길동";
+			String mid= "�솉湲몃룞";
 			int a= 10;
 			int b= 25;
 			System.out.println(mid);
-			System.out.println("안녕");
+			System.out.println("�븞�뀞");
+			String slf="sdfnaonwo";
 	}
 
 }
